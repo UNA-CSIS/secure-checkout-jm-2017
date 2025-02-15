22e9deb3349cc134c1712ea84d0cc3bbeb8b7582
Janet A. Martinez <149624779+jm-2017@users.noreply.github.com>
2025-02-14 23:27:01 -0600

    Update login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <body>
+
+        <form action="match.php" method="post">
+            Name: <input type="text" name="name" />
+            Password: <input type="password" name="pwd" />
+            Remember Me: <input type="checkbox" name="remember" value="ON" />
+            <input type="submit" />
+        </form>
+
+    </body>
+</html>
